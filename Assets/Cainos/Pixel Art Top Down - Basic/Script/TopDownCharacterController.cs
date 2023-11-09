6d533f511a9c1c22376ad789fb97ba26bd746caf
emiliagit <emisosa2002@gmail.com>
2023-11-08 21:31:00 -0300

    canvas

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -10,8 +10,12 @@ namespace Cainos.PixelArtTopDown_Basic
 
         private Animator animator;
 
+        private Rigidbody2D rb;
+
         private void Start()
         {
+            rb = GetComponent<Rigidbody2D>();
+
             animator = GetComponent<Animator>();
         }
 
@@ -19,27 +23,11 @@ namespace Cainos.PixelArtTopDown_Basic
         private void Update()
         {
             Vector2 dir = Vector2.zero;
-            if (Input.GetKey(KeyCode.A))
-            {
-                dir.x = -1;
-                animator.SetInteger("Direction", 3);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                dir.x = 1;
-                animator.SetInteger("Direction", 2);
-            }
+            float horizontalMovement = Input.GetAxis("Horizontal");
+            float verticalMovement = Input.GetAxis("Vertical");
 
-            if (Input.GetKey(KeyCode.W))
-            {
-                dir.y = 1;
-                animator.SetInteger("Direction", 1);
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                dir.y = -1;
-                animator.SetInteger("Direction", 0);
-            }
+            Vector2 movement = new Vector2(horizontalMovement, verticalMovement);
+            rb.velocity = movement * speed;
 
             dir.Normalize();
             animator.SetBool("IsMoving", dir.magnitude > 0);
